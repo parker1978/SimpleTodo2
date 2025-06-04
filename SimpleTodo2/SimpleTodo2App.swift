a521ec8eb3188701ba6542b47dcaf0ac66157cd1
Stephen Parker <parker1978@me.com>
2025-06-04 14:28:38 -0500

    Initial Commit

---
+++ SimpleTodo2/SimpleTodo2App.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodo2App.swift
+//  SimpleTodo2
+//
+//  Created by Stephen Parker on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
